fd3a7cda1ebd1943819593e3f8d307a4168627ce
Brendan Heywood <brendan.heywood@gmail.com>
2011-11-20 14:02:16 +1100

    converted to browser action and added proper logo

--- contentscript.js
+++
@@ -1,6 +0,0 @@
-/*
- * This loads on certain pages and detects wether it is a github repo page
- * If so then it shows the page action to show the issues board
- */
-
-chrome.extension.sendRequest('showIcon', function(response) {});
